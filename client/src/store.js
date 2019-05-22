32622d36b0bf7b702d3b71421a80adf17af38d02
Jungyu Kim <jgtony@gmail.com>
2019-05-22 17:44:22 +0900

    result data regroup chart update

--- client/src/store.js
+++ client/src/store.js
@@ -484,8 +484,8 @@ export const store = new Vuex.Store({
         let tmpResult = state.tmpNewDataSetObj;
         let tmpFailedResult = state.tmpFailedResult;
         let tmpTunning = state.tmpTunningData;
-        let resultPercent = ((3*tmpResult.length) / 10); // ((3*tmpResult.length) / 10)
-        let resultTunning = ((3*tmpTunning.length) / 10);
+        let resultPercent = ((3*tmpResult.length) / 20); // ((3*tmpResult.length) / 10)
+        let resultTunning = ((3*tmpTunning.length) / 20);
 
         console.log("length: " + resultPercent + "%");
         //console.log("failed_length: " + 100-resultPercent + "%");
